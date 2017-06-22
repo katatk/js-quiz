a83fd76060afc1adfa17df4d4d65adc4c2c77522
Kat <katrinaatk@gmail.com>
2017-06-22 14:46:14 +1200

    maps both adding markers. mapbox gets marker based on user location straight away. maps show only locations specific to them

--- geomapr/js/script.js
+++ geomapr/js/script.js
@@ -1,68 +1,4 @@
-// init gmap
-var gmap;
 
-// create google map
-function initMap() {
-    // set intial zoom to zoomed in
-    var mapOptions = {
-        zoom: 2,
-        center: {
-            lat: 0,
-            lng: 0
-        }
-    }
-
-    // initial position for map
-    gmap = new google.maps.Map(document.getElementById('gmap'), mapOptions);
-}
-
-
-// init mapbox
-
-var mapLeafet;
-
-function createMapbox() {
-
-    L.mapbox.accessToken = 'pk.eyJ1Ijoia2F0YWsiLCJhIjoiY2o0Njc0ZGJuMHByNDMybzJnc2Z4NWVsaSJ9.UiTN4aKH6VFzKyGTBQBvCw';
-
-    mapLeaflet = L.mapbox.map('mapbox', 'mapbox.light').setView([37.8, -96], 4);
-
-    L.marker([38.913184, -77.031952]).addTo(mapLeaflet);
-
-    mapLeaflet.scrollWheelZoom.disable();
-}
-
-createMapbox();
-
-
-// map functions
-
-// mapbox functions
-
-function addMarkerLeaflet(lat, lng) {
-    L.marker([lat, lng]).addTo(mapLeaflet);
-}
-
-// toggle mapbox
-document.getElementById("change-map").addEventListener("click", function () {
-    // get containing divs for maps
-    var gmapContainer = document.getElementById("gmap");
-    var mapboxContainer = document.getElementById("mapbox");
-
-    // if google map already showing, load mapbox and update button to gmaps
-    if (this.innerHTML == "Load Mapbox") {
-        this.innerHTML = "Load Google Maps";
-        gmapContainer.style.display = "none";
-        mapboxContainer.style.display = "block";
-
-        // if map box already showing, load gmaps and update button to mapbox
-    } else {
-        this.innerHTML = "Load Mapbox";
-        gmapContainer.style.display = "block";
-        mapboxContainer.style.display = "none";
-    }
-
-});
 
 // location object
 var ipLocation = {
@@ -126,22 +62,12 @@ var root = 'https://freegeoip.net/json/';
 // get the input field
 var inputField = document.getElementById('ip-address');
 
-// prevent page from refreshing when form is submitted
-function preventRefresh() {
-    inputField.addEventListener('keypress', function (event) {
-        if (event.keyCode == 13) {
-            event.preventDefault();
-        }
-    });
-}
-
 // search function
 function search() {
     // get the what is typed into the search box
     var usersearch = inputField.value;
     // make the AJAX request, pass in URL and callback function
     loadDoc(root + usersearch, getValues);
-    preventRefresh();
 }
 
 // get vistor's IP right away 
@@ -150,6 +76,7 @@ function search() {
 // search when enter is pressed
 inputField.onkeydown = function (e) {
     if (e.keyCode == 13) {
+        e.preventDefault();
         search();
     }
 };
@@ -158,7 +85,10 @@ inputField.onkeydown = function (e) {
 var searchBtn = document.getElementById("search");
 
 // search when button is clicked
-searchBtn.addEventListener('click', search);
+searchBtn.onclick = function (e) {
+    e.preventDefault();
+    search();
+}
 
 
 /*function createClusters() {
@@ -206,11 +136,15 @@ function getValues(xhttp) {
         lng: data.longitude
     });
 
-    // run new markers function (gmaps)
-    addMarkers();
+    // add new markers (if gmaps)
+    if (isGmap) {
+        addMarkers();
+    }
 
-    // add markers (mapbox)
-    addMarkerLeaflet(data.latitude, data.longitude);
+    // add markers (if mapbox)
+    if (isMapbox) {
+        addMarkerLeaflet(locations[locations.length - 1].lat, locations[locations.length - 1].lng);
+    }
 
     //run marker cluster function
     /* createClusters();*/
@@ -231,8 +165,16 @@ var markerCluster;
 // build markers function
 // loops through locations array and retruns a new marker for each location object in the array
 function addMarkers() {
+
+    /* var positionObj = {
+        lat: locations[locations.length - 1][0],
+        lng: locations[locations.length - 1][1]
+    };
+*/
     var marker = new google.maps.Marker({
-        position: locations[locations.length - 1],
+        position: locations[locations.length - 1]
+            /*  position: positionObj*/
+            ,
         map: gmap
     });
 
@@ -292,6 +234,71 @@ function clearMap() {
 }
 
 
-/* ==========
-Mapbox
-============= */
+// init gmap
+var gmap;
+
+function initMap() {
+    // set intial zoom to zoomed in
+    var mapOptions = {
+        zoom: 2,
+        center: {
+            lat: 0,
+            lng: 0
+        }
+    }
+
+    // initial position for map
+    gmap = new google.maps.Map(document.getElementById('gmap'), mapOptions);
+}
+
+// init mapbox
+var mapLeafet;
+
+function createMapbox() {
+
+    L.mapbox.accessToken = 'pk.eyJ1Ijoia2F0YWsiLCJhIjoiY2o0Njc0ZGJuMHByNDMybzJnc2Z4NWVsaSJ9.UiTN4aKH6VFzKyGTBQBvCw';
+
+    mapLeaflet = L.mapbox.map('mapbox', 'mapbox.light').setView([0, 0], 2);
+
+    mapLeaflet.scrollWheelZoom.disable();
+}
+
+// create mapbox on page load + add marker for user location straight away
+window.addEventListener("load", function(){
+    createMapbox();
+    search();
+});
+
+// mapboc add markers
+function addMarkerLeaflet(lat, lng) {
+    L.marker([lat, lng]).addTo(mapLeaflet);
+}
+
+// toggle between maps, starting with mapbox on and gmaps off
+
+var isGmap = false;
+var isMapbox = true;
+
+document.getElementById("change-map").addEventListener("click", function () {
+    // get containing divs for maps
+    var gmapContainer = document.getElementById("gmap");
+    var mapboxContainer = document.getElementById("mapbox");
+
+    // if google map already showing, load mapbox and update button to gmaps
+    if (this.innerHTML == "Load Mapbox") {
+        this.innerHTML = "Load Google Maps";
+        gmapContainer.style.display = "none";
+        mapboxContainer.style.display = "block";
+        isGmap = false;
+        isMapbox = true;
+    
+        // if map box already showing, load gmaps and update button to mapbox
+    } else {
+        this.innerHTML = "Load Mapbox";
+        gmapContainer.style.display = "block";
+        mapboxContainer.style.display = "none";
+        isGmap = true;
+        isMapbox = false;
+    }
+
+});
